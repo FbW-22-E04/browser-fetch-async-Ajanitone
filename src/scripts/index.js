6dd425cfa6e25771b7efd08cfdd11513097f600d
Ajanitone <ajanitone@freenet.de>
2022-09-29 15:02:19 +0200

    initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,53 @@
-import "../styles/main.scss";
-import "babel-polyfill"
+//import "../styles/main.scss";
+//import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
 let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
+  window.setTimeout(function () {
+    resolve(modal);
+  }, 1000 * 60);
 });
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+// promiseOfModal.then(function (val) {
+//   console.log("User has been on the page for 60 seconds");
+//   val.style.display = "block";
+// });
+
+async function promiseOfModal2() {
+  let val = await promiseOfModal;
+  console.log("User has been on the page for 60 seconds");
+  val.style.display = "block";
+  console.log(val);
+}
+
+promiseOfModal2();
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
 
+// const modal = document.getElementById("myModal");
+// let promiseOfModal = new Promise(function (resolve) {
+//   window.setTimeout(function () {
+//     resolve(modal);
+//   }, 1000);
+// });
+
+// promiseOfModal.then(function (val) {
+//   console.log("User has been on the page for 60 seconds");
+//   val.style.display = "block";
+// });
+
+// modal.addEventListener("click", (e) => {
+//   switch (e.target.className) {
+//     case "close":
+//     case "modal":
+//       modal.style.display = "none";
+//       break;
+//   }
+// });
